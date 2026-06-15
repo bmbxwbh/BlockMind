39b20d0ca506a252b3a8f53b1085efb39ecb896a
bmbxwbh <bmbxwbh@users.noreply.github.com>
2026-06-15 15:43:47 +0800

    debug: minimal window - just TextBlock to test rendering

--- desktop/src/BlockMind.Desktop/Views/MainWindow.axaml.cs
+++ desktop/src/BlockMind.Desktop/Views/MainWindow.axaml.cs
@@ -1,6 +1,4 @@
 using Avalonia.Controls;
-using Avalonia.Input;
-using BlockMind.Desktop.ViewModels;
 using System;
 using System.IO;
 
@@ -8,37 +6,16 @@ namespace BlockMind.Desktop.Views;
 
 public partial class MainWindow : Window
 {
-    private static readonly string LogFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "blockmind-debug.log");
-
-    private static void Log(string msg)
-    {
-        try { File.AppendAllText(LogFile, $"[{DateTime.Now}] [MainWindow] {msg}\n"); } catch { }
-    }
-
     public MainWindow()
     {
-        Log("MainWindow constructor start");
         try
         {
             InitializeComponent();
-            Log("InitializeComponent done");
         }
         catch (Exception ex)
         {
-            Log($"InitializeComponent failed: {ex.Message}");
-            Log($"Stack: {ex.StackTrace}");
-            throw;
-        }
-    }
-
-    private void NavBorder_PointerPressed(object? sender, PointerPressedEventArgs e)
-    {
-        if (sender is Border border && border.Tag is NavItemViewModel item)
-        {
-            if (DataContext is MainWindowViewModel vm)
-            {
-                vm.SelectedNav = item;
-            }
+            var logFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "blockmind-debug.log");
+            File.AppendAllText(logFile, $"[{DateTime.Now}] [MainWindow] ERROR: {ex}\n");
         }
     }
 }
